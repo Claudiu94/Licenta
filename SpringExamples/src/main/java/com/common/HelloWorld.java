915f0707eddbc2bd03a00936c6280f84676b2666
Claudiu94 <claudiu.zafi@gmail.com>
2017-04-02 16:50:38 +0300

    added .gitignore file

--- SpringExamples/src/main/java/com/common/HelloWorld.java
+++ SpringExamples/src/main/java/com/common/HelloWorld.java
@@ -4,4 +4,8 @@ package com.mkyong.common;
  * Created by cozafiu on 08.03.2017.
  */
 public class HelloWorld {
+
+    public void printMessage() {
+        System.out.println( "Hello World!" );
+    }
 }
---
+++ SpringExamples/src/main/java/com/common/MainApp.java
@@ -0,0 +1,7 @@
+package com.common;
+
+/**
+ * Created by claudiu on 02.04.2017.
+ */
+public class MainApp {
+}
