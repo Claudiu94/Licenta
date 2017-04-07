10f12ea008e8db3fc13a280ebefd313a58c6f145
Claudiu94 <claudiu.zafi@gmail.com>
2017-04-07 07:33:12 +0300

    rest server started implementation

---
+++ FinanceService/src/main/java/mainservices/Application.java
@@ -0,0 +1,21 @@
+package mainservices;
+
+/**
+ * Created by claudiu on 07.04.2017.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+import java.io.IOException;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) throws IOException {
+//        SpringApplication.run(Application.class, args);
+
+        System.out.println(YahooFinance.get("CL=F").getStats());
+    }
+}
