fa19b3a72ddceda4a535cd11828c1ff8b9cbd20a
Claudiu94 <claudiu.zafi@gmail.com>
2017-06-11 12:05:59 +0300

    added exchange rate for USD/EUR

--- FinanceClient/public/javascripts/home.js
+++ FinanceClient/public/javascripts/home.js
@@ -1,47 +1,76 @@
 $(document).ready(function(){
 	var localObj = JSON.parse($("#myLocalDataObj").val());
 
-	Highcharts.chart('container', {
+	$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=usdeur.json&callback=?', function (data) {
 
-		title: {
-			text: 'Solar Employment Growth by Sector, 2010-2016'
-		},
+    var startDate = new Date(data[data.length - 1][0]), // Get year of last data point
+        minRate = 1,
+        maxRate = 0,
+        startPeriod,
+        date,
+        rate,
+        index;
 
-		yAxis: {
-			title: {
-				text: 'Number of Employees'
-			}
-		},
-		legend: {
-			layout: 'vertical',
-			align: 'right',
-			verticalAlign: 'middle'
-		},
+    startDate.setMonth(startDate.getMonth() - 3); // a quarter of a year before last data point
+    startPeriod = Date.UTC(startDate.getFullYear(), startDate.getMonth(), startDate.getDate());
 
-		plotOptions: {
-			series: {
-				pointStart: 2010
-			}
-		},
+    for (index = data.length - 1; index >= 0; index = index - 1) {
+        date = data[index][0]; // data[i][0] is date
+        rate = data[index][1]; // data[i][1] is exchange rate
+        if (date < startPeriod) {
+            break; // stop measuring highs and lows
+        }
+        if (rate > maxRate) {
+            maxRate = rate;
+        }
+        if (rate < minRate) {
+            minRate = rate;
+        }
+    }
 
-		series: [{
-			name: 'Installation',
-			data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-		}, {
-			name: 'Manufacturing',
-			data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-		}, {
-			name: 'Sales & Distribution',
-			data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-		}, {
-			name: 'Project Development',
-			data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-		}, {
-			name: 'Other',
-			data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-		}]
+    // Create the chart
+    Highcharts.stockChart('container', {
 
-	});
+        rangeSelector: {
+            selected: 1
+        },
+
+        title: {
+            text: 'USD to EUR exchange rate'
+        },
+
+        yAxis: {
+            title: {
+                text: 'Exchange rate'
+            },
+            plotLines: [{
+                value: minRate,
+                color: 'green',
+                dashStyle: 'shortdash',
+                width: 2,
+                label: {
+                    text: 'Last quarter minimum'
+                }
+            }, {
+                value: maxRate,
+                color: 'red',
+                dashStyle: 'shortdash',
+                width: 2,
+                label: {
+                    text: 'Last quarter maximum'
+                }
+            }]
+        },
+
+        series: [{
+            name: 'USD to EUR',
+            data: data,
+            tooltip: {
+                valueDecimals: 4
+            }
+   		}]
+   	})
+})
 
 	$(".table-row").click(function(e){
 		var id = e.target.parentNode.id;
